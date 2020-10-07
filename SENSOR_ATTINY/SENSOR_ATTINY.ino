9ffa46cc65ad1b627f7a692d7942ec9f30199caf
sam-holm <samueldholm@gmail.com>
2020-10-08 10:43:55 +1300

    ATtiny sampling from temp sensor on final PCB

--- SENSOR_ATTINY/SENSOR_ATTINY.ino
+++ SENSOR_ATTINY/SENSOR_ATTINY.ino
@@ -1,17 +1,16 @@
 // the 7-bit address (remember to change this when adapting this example)
 #define I2C_SLAVE_ADDRESS 0x04
-
 // Get this from https://github.com/rambo/TinyWire
 #include <TinyWireS.h>
-
-char reading = 1;
-
-void requestEvent(){  
-  TinyWireS.send(reading);
-}
+#include "dht.h"
+#define DHTPIN 4
+dht DHT;
+int reading;
 
 void setup()
 {
+    pinMode(1, OUTPUT);
+    pinMode(4, INPUT);
     //Serial.begin(9600);
     TinyWireS.begin(I2C_SLAVE_ADDRESS);
     //Sets up the onRequest function (what we do if asked to send something).
@@ -20,5 +19,17 @@ void setup()
 
 void loop()
 {
-
+  int chk = DHT.read11(DHTPIN);
+  //reading[0] = dht.readHumidity();
+  reading = DHT.temperature;
+  delay(2000); 
 }
+
+void requestEvent(){  
+  TinyWireS.send(reading);
+  digitalWrite(1,HIGH);
+  delay(10);
+  digitalWrite(1,LOW);
+}
+
+//Just need to get humidity and temp come through hard g
